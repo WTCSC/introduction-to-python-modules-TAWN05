3f3f12c5816631b0465506b54f03dc4cc0cd3ae7
tawnos <Jacobrcasey135@gmail.com>
2024-09-10 20:17:00 +0000

    finished with comments

--- average.py
+++ average.py
@@ -1,8 +1,21 @@
 import text_utils
+import math
 def average_words():
-    num_words = text_utils.count_words('sample.txt')
-    """nlines = text.count('\n')"""
-    nlines = text_utils.count_lines('sample.txt')
-    average = (num_words)/(nlines)
-    message = (f'Average words per line: {average}')
-    return message
+    # Defines file = opening and read mode of 'sample.txt'.
+    file = open('sample.txt', 'r')
+    # Defines text = the context of file.
+    text = file.read()
+    # Defines num _words = to the text_utils function count_words in context of text.
+    num_words = text_utils.count_words(text)
+    # Defines nlines = to the text_utils function count_lines in context of text.
+    nlines = text_utils.count_lines(text)
+    # Defines average = to num_words divided by nlines.
+    average = (num_words / nlines) 
+    # Defines round_average = to the floor or rounded down of the varable. 
+    round_average = math.floor(average)
+    # Closes the file that has been opened in the function.
+    file.close()
+    # Returns the formatted string (the desired output for average_words).
+    return f'Average words per line: {round_average}'
+# Call's average_words so it can be tested and prints the resault in terminal.
+print(average_words())
--- text_utils.py
+++ text_utils.py
@@ -1,16 +1,24 @@
 def count_chars(text):
+    # Returns the lenth of text
     return len(text)
 
 
 def count_words(text):
+    # returns length of text split into words.
     return len(text.split())
     
 def count_sentences(text):
+    # Counts periods and sets the number equal to i.
     i = text.count('.')
+    # Counts bang's and sets the number equal to b.
     b = text.count('!')
+    # Counts question marks and sets the number equal to a.
     a = text.count('?')
+    # Defines num_sentances = to the sum of i, b, and a.
     num_sentences = (i + b + a)
+    # Returns the varable num_sentences
     return num_sentences
 
 def count_lines(text):
-    return text.count('\n')
+    # Returns the number of lines in context of text
+    return len(text.readlines())
